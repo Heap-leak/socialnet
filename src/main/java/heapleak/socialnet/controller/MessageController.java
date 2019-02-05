7114ecf8b043ed203a8a6e196a3a090bfa41ef9d
Evgenii Kulemin <evgenii.kulemin@NetCracker.com>
2019-02-05 15:45:48 +0300

    Message controller
    Added methods for: Create, Delete, Update

--- src/main/java/heapleak/socialnet/controller/MessageController.java
+++ src/main/java/heapleak/socialnet/controller/MessageController.java
@@ -2,11 +2,7 @@ package heapleak.socialnet.controller;
 
 
 import heapleak.socialnet.exceptions.NotFoundException;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,10 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>(){{put("id:", "1"); put("massage", "First");}});
-        add(new HashMap<String, String>(){{put("id:", "2"); put("massage", "Second");}});
-        add(new HashMap<String, String>(){{put("id:", "3"); put("massage", "Third");}});
+    private int counter = 4;
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>(){{
+        add(new HashMap<String, String>(){{put("id", "1"); put("massage", "First");}});
+        add(new HashMap<String, String>(){{put("id", "2"); put("massage", "Second");}});
+        add(new HashMap<String, String>(){{put("id", "3"); put("massage", "Third");}});
     }};
 
     @GetMapping
@@ -29,9 +26,35 @@ public class MessageController {
 
     @GetMapping("{id}")
     public Map<String, String> getOne(@PathVariable String id){
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter( message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
     }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message){
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message){
+        Map<String, String> messageFromDb = getMessage(id);
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Map<String, String> message = getMessage(id);
+        messages.remove(message);
+    }
+
 }
